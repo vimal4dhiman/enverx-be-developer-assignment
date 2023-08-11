adfb5494ead2d9a49f55f13e645cc0aaff028e18
Vimal Dhiman <vimal4dhiman@gmail.com>
2023-08-11 13:54:13 +0530

    Setup-up and configure DynamoDB credentials

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const AWS = require("aws-sdk");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
+
+const config = JSON.parse(fs.readFileSync("config.json"));
+
+// Configuring AWS SDK with credentials
+AWS.config.update({
+  region: config.database.region,
+  accessKeyId: config.database.accessKeyId,
+  secretAccessKey: config.database.secretAccessKey,
+});
+
+// Setting up dyamodb client
+const dynamodb = AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json());
 
 app.listen(PORT, (error) => {
   if (!error) {
